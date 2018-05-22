dbc070b4f4171d038094fc7cdb3d688d86fcfe6e
MrK <crodriguezq91@gmail.com>
2018-05-21 21:40:01 -0500

    Updating day 4 excersises

--- day-4/codility-MaxCounters.js
+++ day-4/codility-MaxCounters.js
@@ -25,3 +25,34 @@ function solution(N, A) {
 
     return counters;
 }
+
+// final solution: https://app.codility.com/demo/results/training28JGPT-ZKQ/
+// copy from https://app.codility.com/demo/results/trainingQ3KEC6-S4T/
+
+function solution(N, A) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    var counters = new Array(N);
+    var max = 0, min = 0;
+    for (var i=0; i<N; i++){
+        counters[i] = 0;
+    }
+
+    if(A.length < 1) {
+        return counters;
+    }
+
+    for(var i=0; i<A.length; i++){
+        if(A[i] > N){
+            min = max;
+        } else {
+            counters[A[i]-1] = Math.max(counters[A[i]-1], min);
+            max = Math.max(++counters[A[i]-1], max);
+        }
+    }
+
+    for(var i=0; i<counters.length; i++) {
+        counters[i] = Math.max(counters[i], min);
+    }
+
+    return counters;
+}
